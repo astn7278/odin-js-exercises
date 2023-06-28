4d8df84f4cc851e3256ba1e281cb5e9823a37a35
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-06-27 21:04:34 -0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    splitWord = word.split("");
+    reverseArray = splitWord.reverse();
+    reverseWord = reverseArray.join("");
+    return(reverseWord);
 };
 
 // Do not edit below this line
