c49c7328237811b6e4476bf229625c236f74b815
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-06-27 20:39:08 -0700

    Complete exercise 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, x) {
+    let newString = '';
+    if (x < 0) {
+        return('ERROR');
+    }
+    else {
+        for (let i = 0; i < x; i++){
+            newString += string;
+        }
+        return(newString)
+    }
 };
 
 // Do not edit below this line
