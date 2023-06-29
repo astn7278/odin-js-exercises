0c78d13aab4922681c188a351f44b742f3f9f519
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-06-28 22:55:02 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if ((num1 > 0) && (num2 > 0)) {
+        if(num1 < num2)  {
+            let total = 0;
+            for(i = num1; i <= num2; i++) {
+            total += i;
+            }
+        return total;
+        } else {
+            let total = 0;
+            for(i = num2; i <= num1; i++) {
+            total += i;
+            }
+        return total;
+        
+        }
+    } else return 'ERROR'
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
