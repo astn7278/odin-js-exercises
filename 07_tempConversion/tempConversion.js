d960548df74df6b648a60b439532737a8449b887
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-06-29 22:59:37 -0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  f = ((f - 32) * 5 / 9);
+  rounded = Math.round(f * 10) / 10
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  c = (c * 1.8 + 32);
+  rounded = Math.round(c * 10) / 10
+  return rounded;
 };
 
 // Do not edit below this line
@@ -9,3 +15,15 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+/*
+
+Fahrenheit to Celsius
+x °F ≘ (x − 32) × 5/9 °C 
+
+Celsius to Fahrenheit
+x °C ≘ (x × 9/5 + 32) °F
+
+*/
+
