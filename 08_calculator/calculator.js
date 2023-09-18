03339977830ae2d88fc7c5f6c40d69e3792335ad
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-09-18 10:34:14 -0700

    Complete factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -13,9 +13,17 @@ const multiply = function (...args) {
   return args.reduce((accum, current) => accum * current, 1);
 };
 
-const power = function () {};
+const power = function (num1, exp1) {
+  return num1 ** exp1;
+};
 
-const factorial = function () {};
+const factorial = function (n) {
+  let myArray = [];
+  for (let i = n; i > 0; i--){
+    myArray.push(i);
+  };
+  return myArray.reduce((accum, current) => accum * current, 1);
+};
 
 // Do not edit below this line
 module.exports = {
