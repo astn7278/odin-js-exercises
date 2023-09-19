9d166784d68042b0b1d78bae37c89f4a5231cda0
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-09-18 23:28:03 -0700

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+    if (count < 0) return "OOPS";
+    let fibPart = [0, 1];
+    let fibResult;
+    for (let index = 1; index < count; index++) {
+        fibResult = fibPart[index] + fibPart[index - 1];
+        fibPart.push(fibResult);
+    }
+    return fibPart[count];
 };
 
 // Do not edit below this line
