75d8ca309acb2ef5799c2129a3a5e4d592da6a54
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-09-25 21:43:57 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
-
+findTheOldest = function (arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        return deathYear(currentPerson) - currentPerson.yearOfBirth >
+            deathYear(oldestPerson) - oldestPerson.yearOfBirth
+            ? currentPerson
+            : oldestPerson;
+    });
 };
+
+const currentYear = new Date().getFullYear();
+const deathYear = function (obj) {
+    return obj.hasOwnProperty("yearOfDeath") ? obj.yearOfDeath : currentYear;
+};
+
+/*
+const findTheOldest = (arr) => {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        if ((currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth)) {
+            return currentPerson;
+        }
+        else {
+            return oldestPerson;
+        }
+    })
+}
+
+let result = arr.reduce((sum, current) => sum + current, 0);
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
